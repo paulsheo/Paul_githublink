6acf5488d908bba83fbfaeb92b84e533c679ee9b
paulsheo <113318941+paulsheo@users.noreply.github.com>
2022-10-24 11:01:04 -0700

    Add files via upload

---
+++ 4-group-live-share/object/example.js
@@ -0,0 +1,22 @@
+/**
+ * 
+17-2 Create a student object that has the first name value of Mark, last name value as Jacob, age as 31, and the id as A001234. Then initialise the word first to the variable sentence. Then assign it on another line to say the following: 
+
+The new employee __first name__   __last name__ is __age__ years old. The employee ID is __empyloyee ID__.
+
+Then console log the sentence.
+
+ */
+
+var employee = { 
+    firstName: "Mark",
+    lastName: "Jacob",
+    age: 31,
+    id: "A001234"
+
+};
+
+var sentence;
+sentence = "The new employee" + employee.firstName + " " + employee.lastName + " is " + employee.age + "years old. The employee ID is " + employee.id + ".";
+
+console.log(sentence);
